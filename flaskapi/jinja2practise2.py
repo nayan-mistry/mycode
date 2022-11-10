4e59c5a9458f423520851afc594d563af3ad9b94
nayan-mistry <mistry_nayan@yahoo.com>
2022-11-10 08:48:20 +0000

    sql lite & jinja2 practise

---
+++ flaskapi/jinja2practise2.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/<username>")
+def index(username):
+    return render_template("helloname.html", name=username)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2224)
